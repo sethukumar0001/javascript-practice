2cba2204c7e1a6da64fa18db9fdc58dc36c8d937
sethukumar0001 <boylooksrock@gmail.com>
2020-04-20 23:31:26 +0530

    triangle pyramid

--- pyramid1.js
+++ pyramid1.js
@@ -3,6 +3,11 @@
 
 //squre pyramid:
 
+// 1234
+// 1234
+// 1234
+// 1234
+
 function Pyramid1() {
     let rows = 5;
     let cols = 5;
@@ -17,4 +22,25 @@ function Pyramid1() {
         output = '';
     }
 }
-Pyramid1();
+// Pyramid1();
+
+//triangle pyramid
+
+// 1
+// 12
+// 123
+// 1234
+
+const Pyramid2 = () =>{
+let rows = 5;
+let output = "";
+
+for (var i=1;i< rows;i++){
+    for (j=1;j<= i;j++){
+        output = output+j
+    }
+    console.log(output);
+    output = ""
+}
+}
+// Pyramid2();
